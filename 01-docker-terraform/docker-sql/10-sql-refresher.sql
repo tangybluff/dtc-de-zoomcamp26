9caa0b7c50e61f967711f83682dfbade7d386de3
tangybluff <ahmedbouallou@gmail.com>
2026-02-19 15:30:42 +0100

    Renamed files

---
+++ 01-docker-terraform/docker-sql/10-sql-refresher.sql
@@ -0,0 +1,28 @@
+--Select the first 5 rows from the table.
+SELECT * FROM yellow_taxi_trips_2021_1 LIMIT 5;
+
+--Find the total number of trips where the passenger count is 2.
+
+
+--Calculate the average fare amount for all trips.
+
+--List all unique payment types used in the data.
+
+
+--Find the trip with the highest total amount.
+
+
+--Count how many trips had a tip amount greater than $10.
+
+
+--Show the number of trips for each day (group by pickup date).
+
+
+--Find the average trip distance for each passenger count.
+
+
+--List the top 3 drop-off locations (DOLocationID) by number of trips.
+
+
+--Find all trips where the trip distance is less than 1 mile but the total amount is more than $50.
+
